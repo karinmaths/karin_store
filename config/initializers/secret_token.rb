c53f72943466b70598916a5fe7553d2f2236956a
karinmaths <karin.maths@gmail.com>
2012-12-27 10:11:08 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KarinStore::Application.config.secret_token = '3549640bb78b199bc116f509c558b4752563ea78b66a394084be5e2a9b0d734dd7f3d6182e2a3756f4743a8cb7e5466b2a1cfc01fba30abd6f8a66dbdab42acd'
